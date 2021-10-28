52e902dce90d02365429b31673aff4768701e7ef
Tub <nilstobiasekman@gmail.com>
2021-10-28 12:46:17 +0200

    formatting

--- database.js
+++ database.js
@@ -1,16 +1,16 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose()
 
-const DBSOURCE = "quiz.db"
-
+const DBSOURCE = 'quiz.db'
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
         console.log('Connected to the SQlite database.')
-        db.run(`CREATE TABLE user (
+        db.run(
+            `CREATE TABLE user (
             id INTEGER PRIMARY KEY,
             email TEXT,
             username TEXT,
@@ -18,18 +18,26 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             user_level TEXT,
             questions_answered INTEGER,
             questions_correct INTEGER
-            )`,(err) => {
-        if (err) {
-            // Table already created
-        }else{
-            // Table just created, creating some rows
-            const insert = 'INSERT INTO user (email, username, password, user_level, questions_answered, questions_correct) VALUES (?,?,?,?,?,?)';
-            db.run(insert, ["bosse@hotbrev.com", "BossemanneN", "guest123", "user",22,2])
-        }
-    })
+            )`,
+            (err) => {
+                if (err) {
+                    // Table already created
+                } else {
+                    // Table just created, creating some rows
+                    const insert =
+                        'INSERT INTO user (email, username, password, user_level, questions_answered, questions_correct) VALUES (?,?,?,?,?,?)'
+                    db.run(insert, [
+                        'bosse@hotbrev.com',
+                        'BossemanneN',
+                        'guest123',
+                        'user',
+                        22,
+                        2,
+                    ])
+                }
+            }
+        )
     }
 })
 
-
 module.exports = db
-
--- server.js
+++ server.js
@@ -1,143 +1,131 @@
-const express = require("express");
-const app = express();
-const cors = require('cors');
-const db = require("./database.js");
-const bcrypt = require('bcrypt');
-
-
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const db = require('./database.js')
+const bcrypt = require('bcrypt')
 
 app.use(cors())
 app.use(express.static('public'))
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const HTTP_PORT = 3000;
+const HTTP_PORT = 3000
 
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
-});
+    console.log('Server running on port %PORT%'.replace('%PORT%', HTTP_PORT))
+})
 
-app.get("/api/quiz", (req, res, next) => {
-    const sql = "select * from quiz";
-    const params = [];
+app.get('/api/quiz', (req, res, next) => {
+    const sql = 'select * from quiz'
+    const params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ error: err.message })
+            return
         }
         res.json({
-            "message":"success",
-            "quiz":rows
+            message: 'success',
+            quiz: rows,
         })
-      });
-});
+    })
+})
 
-
-app.get("/api/quiz/:id", (req, res, next) => {
-    const sql = "select * from quiz where id = ?";
-    const params = [req.params.id];
+app.get('/api/quiz/:id', (req, res, next) => {
+    const sql = 'select * from quiz where id = ?'
+    const params = [req.params.id]
     db.get(sql, params, (err, row) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ error: err.message })
+            return
         }
         res.json({
-            "message":"success",
-            "quiz":row
+            message: 'success',
+            quiz: row,
         })
-      });
-});
+    })
+})
 
-app.get("/api/quizzes/:nr", (req,res,next) => {
-    const sql = "SELECT * FROM quiz ORDER BY RANDOM() LIMIT ?";
-    const params = [req.params.nr];
+app.get('/api/quizzes/:nr', (req, res, next) => {
+    const sql = 'SELECT * FROM quiz ORDER BY RANDOM() LIMIT ?'
+    const params = [req.params.nr]
     db.all(sql, params, (err, rows) => {
         if (err) {
-            res.status(400).json({"error":err.message});
-            return;
+            res.status(400).json({ error: err.message })
+            return
         }
         res.json({
-            "message":"success",
-            "quiz":rows
+            message: 'success',
+            quiz: rows,
         })
-    });
-});
+    })
+})
 
-
-app.get("/api/quizzes/:category/:type/:nr", (req,res,next) => {
-    const sql = "SELECT * FROM quiz WHERE category = ? AND type = ? ORDER BY RANDOM() LIMIT ?";
-    const params = [req.params.category,req.params.type,req.params.nr];
+app.get('/api/quizzes/:category/:type/:nr', (req, res, next) => {
+    const sql =
+        'SELECT * FROM quiz WHERE category = ? AND type = ? ORDER BY RANDOM() LIMIT ?'
+    const params = [req.params.category, req.params.type, req.params.nr]
     db.all(sql, params, (err, rows) => {
         if (err) {
-            res.status(400).json({"error":err.message});
-            return;
+            res.status(400).json({ error: err.message })
+            return
         }
         res.json({
-            "message":"success",
-            "quiz":rows
+            message: 'success',
+            quiz: rows,
         })
-    });
-});
+    })
+})
 
-
-
-
-
-
-
-
-app.get("/api/user/:username/:password", (req, res, next) => {
-    const sql = "select * from user WHERE username = ?";
-    const params = [req.params.username];
+app.get('/api/user/:username/:password', (req, res, next) => {
+    const sql = 'select * from user WHERE username = ?'
+    const params = [req.params.username]
 
     db.get(sql, params, (err, row) => {
         if (err) {
-            res.status(400).json({"error": err.message});
-            return;
+            res.status(400).json({ error: err.message })
+            return
         }
-        const isValidPass = bcrypt.compareSync(req.params.password, row.password);
-        if(isValidPass){
-            res.json({"message":"success", "user":row})
-        }else{
-            res.json({"message":"invalid username or password"})
+        const isValidPass = bcrypt.compareSync(
+            req.params.password,
+            row.password
+        )
+        if (isValidPass) {
+            res.json({ message: 'success', user: row })
+        } else {
+            res.json({ message: 'invalid username or password' })
         }
-    });
+    })
+})
 
-
-});
-
-
-app.post("/api/user", (req, res, next) => {
-    const errors = [];
-    if (!req.body.username || !req.body.email || !req.body.password){
-        errors.push("Saknar data för att skapa konto");
+app.post('/api/user', (req, res, next) => {
+    const errors = []
+    if (!req.body.username || !req.body.email || !req.body.password) {
+        errors.push('Saknar data för att skapa konto')
     }
     const data = {
         email: req.body.email,
         username: req.body.username,
         password: bcrypt.hashSync(req.body.password, 10),
-        user_level : "user"
-    };
-    const sql = 'INSERT INTO user (email, username, password, user_level) VALUES (?,?,?,?)';
-    const params = [data.email, data.username, data.password, data.user_level];
+        user_level: 'user',
+    }
+    const sql =
+        'INSERT INTO user (email, username, password, user_level) VALUES (?,?,?,?)'
+    const params = [data.email, data.username, data.password, data.user_level]
     db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return
         }
         res.json({
-            "message": "success",
-            "user": data,
-            "id" : this.lastID
+            message: 'success',
+            user: data,
+            id: this.lastID,
         })
-    });
+    })
 })
-
-
-
 
 /*  TODO: put med statistik över antal frågor
 app.put("/api/bok/:id", (req, res, next) => {
@@ -180,7 +168,6 @@ app.delete("/api/bok/:id", (req, res, next) => {
 
 */
 // Root path
-app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
-});
-
+app.get('/', (req, res, next) => {
+    res.json({ message: 'Ok' })
+})
